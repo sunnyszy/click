79c79cd0cac0dcaf0fba62b55ffb6f7a81b3f9dc
Hansen Qian <hansenq@users.noreply.github.com>
2016-04-05 17:31:52 -0400

    Fix more compilation errors

--- elements/tcpudp/deduptcppacket.hh
+++ elements/tcpudp/deduptcppacket.hh
@@ -46,7 +46,7 @@ class DeDupTCPPacket : public Element { public:
   const char *port_count() const		{ return PORTS_1_1X2; }
   const char *processing() const		{ return AGNOSTIC; }
 
-  int configure();
+  int configure(Vector<String> &, ErrorHandler *);
   void add_handlers();
 
   Packet *simple_action(Packet *);
